cb22d350137ce973cfeb5f99c55222609a6aacc4
Git-shouvik <shouvikchowdhury18@gmail.com>
2020-06-07 23:53:08 +0530

    directory_count

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,57 +1,38 @@
 currentdir=$(ls -A | wc -l)
-
 function evaluate {
-  while [[ "$val" -ne "$currentdir" ]]
-do
-#echo "HOW MANY FILES ARE IN THE CURRENT DIRECTORY "
+while [[ "$val" != "$currentdir" ]]; do
 read -p " TAKE A GUESS AGAIN " val
-if [[ -z $val ]]
- then
-  echo "NO Inputs ! "
-  else
-    if [[ $val =~ ^[0-9]*$ ]]
-       then
-        if [[ $val -lt $currentdir ]] 
-           then 
-           echo " the guess is too low "
-        elif [[ $val -gt  $currentdir ]]
-           then
-            echo " the guess is too high "
-         fi
-   else
-   echo " please enter positive integer"
-   val=0
-   fi
-fi
+  if [[ -z $val ]]; then
+     echo "NO Inputs ! "
+  elif ! [[ $val =~ ^[0-9]*$ ]]; then
+     echo " please enter positive integer"
+ else
+       if [[ $val -lt $currentdir ]]; then
+            echo " the guess is too low "
+      elif [[ $val -gt  $currentdir ]]; then
+           echo " the guess is too high "
+        fi
+     fi
 done
-             echo "   CONGRATES YOU  have guessed it  "
-                 echo  "   THANK YOU FOR PLAYING  "
+              echo "   CONGRATES YOU  have guessed it  "; echo  "   THANK YOU FOR PLAYING  "
 }
 flag=0
-while [[ $flag -eq 0 ]]
-do
+while [[ $flag -eq 0 ]]; do
  echo "How Many files are in the current directory "
 read -p "Take a guess" val
-if [[ -z $val ]]
-then
+ if [[ -z $val ]]; then
 echo " inputs please !"
-else
-   let flag=$flag+1
-  if [[ $val =~ ^[0-9]*$ ]]
-   then
-    if [[ $val -ne $currentdir ]]
-    then
-     echo "The guess was either to LOW or HIGH"
-     evaluate
-     else
-    echo "Congratas U have gussed it"
-   fi
+ elif ! [[ $val =~ ^[0-9]*$ ]]; then
+        echo "please enter positive integers" 
  else
- echo "Enter positive integers ! "
-    val=0
-    evaluate
+     if [[ $val != $currentdir ]]; then
+      echo "The guess was either to LOW or HIGH"
+      evaluate
+     else
+        echo "Congratas U have gussed it"
+     fi
+     let flag=$flag+1
   fi
-fi
 done
 
 
